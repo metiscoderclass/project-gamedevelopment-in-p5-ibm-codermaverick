c98e1d76aaf4a295de6ff95d23a5dc446f954a70
issamvandecoderclass <issam.buamran@coderclass.nl>
2017-11-20 12:50:35 +0100

    Add keyevents

--- js/ballen.js
+++ js/ballen.js
@@ -1,6 +1,8 @@
 var xSize = 1520;
 var ySize = 755;
 var bal1;
+var player1_y = 70;
+var player2_y = 70;
 
 function setup(){
   createCanvas(xSize, ySize);
@@ -8,16 +10,29 @@ function setup(){
   bal1.xPos = 21;
   bal1.yPos = 21;
   bal1.radius = 10;
-  bal1.xSpeed = 10;
-  bal1.ySpeed = 10;
+  bal1.xSpeed = 6;
+  bal1.ySpeed = 6;
 }
 
 function draw(){
   background(0);
   bal1.teken();
   bal1.beweeg();
-  rect(30, 20, 20, 125);
-  rect(1470, 20, 20, 125);
+  rect(30, player2_y, 20, 125);
+  rect(1470, player1_y, 20, 125);
+  if (keyIsDown(UP_ARROW)){
+       player1_y -= 8;
+       console.log("Boven");
+     } else if (keyIsDown(DOWN_ARROW)){
+       player1_y += 8;
+       console.log("Beneden");
+     } if (keyIsDown(87)){
+       player2_y -= 8;
+       console.log("Beneden");
+     } else if (keyIsDown(83)){
+       player2_y += 8;
+       console.log("Beneden");
+     }
 }
 
 function Bal(){
